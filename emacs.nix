e96eb53a1d90220c35a8e5f93e16ac23b030d058
Jappie Klooster <jappieklooster@hotmail.com>
2018-08-25 20:54:56 +1000

    Add avy and general

--- emacs.nix
+++ emacs.nix
@@ -4,6 +4,7 @@ let
   myEmacs = pkgs.emacs.override {} ;
   emacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages;
 
+  # https://sam217pa.github.io/2016/09/02/how-to-build-your-own-spacemacs/
   myEmacsConfig = pkgs.writeText "default.el" ''
 ;; initialize package
 
@@ -12,8 +13,20 @@ let
 (eval-when-compile
   (require 'use-package))
 ;; load packages
+
+;;; keybindings
+(use-package general
+  :config
+  (general-define-key "C-'" 'avy-goto-word-1)
+) 
+;;; jump around
+(use-package avy
+  :commands (avy-goto-word-1))
+
+;;; git
 (use-package magit
   :defer
+  :commands (magit-status magit-dispatch-popup)
   :if (executable-find "git")
   :bind (("C-x g" . magit-status)
          ("C-x G" . magit-dispatch-popup)))
@@ -25,6 +38,9 @@ in
       mkdir -p $out/share/emacs/site-lisp
       cp ${myEmacsConfig} $out/share/emacs/site-lisp/default.el
       '')
+    avy
     magit          # Integrate git <C-x g>
     use-package
+  ]) ++ (with epkgs.melpaPackages; [
+    general
   ]))
