b62f9264956ff27562359a188a96272d34baddcd
Jappie Klooster <jappieklooster@hotmail.com>
2018-08-25 20:24:04 +1000

    Load magit by default

--- emacs.nix
+++ emacs.nix
@@ -1,28 +1,30 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  myEmacs = (pkgs.emacs.override {
-    # Use gtk3 instead of the default gtk2
-    withGTK3 = true;
-    withGTK2 = false;
-  }).overrideAttrs (attrs: {
-    # I don't want emacs.desktop file because I only use
-    # emacsclient.
-    postInstall = (attrs.postInstall or "") + ''
-      rm $out/share/applications/emacs.desktop
-    '';
-  });
+  myEmacs = pkgs.emacs.override {} ;
   emacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages;
+
+  myEmacsConfig = pkgs.writeText "default.el" ''
+;; initialize package
+
+(require 'package)
+(package-initialize 'noactivate)
+(eval-when-compile
+  (require 'use-package))
+;; load packages
+(use-package magit
+  :defer
+  :if (executable-find "git")
+  :bind (("C-x g" . magit-status)
+         ("C-x G" . magit-dispatch-popup)))
+    '';
+
 in
   emacsWithPackages (epkgs: (with epkgs.melpaStablePackages; [
+(pkgs.runCommand "default.el" {} ''
+      mkdir -p $out/share/emacs/site-lisp
+      cp ${myEmacsConfig} $out/share/emacs/site-lisp/default.el
+      '')
     magit          # Integrate git <C-x g>
-    zerodark-theme # Nicolas' theme
-  ]) ++ (with epkgs.melpaPackages; [
-    undo-tree      # <C-x u> to show the undo tree
-  ]) ++ (with epkgs.elpaPackages; [
-    auctex         # LaTeX mode
-    beacon         # highlight my cursor when scrolling
-    nameless       # hide current package name everywhere in elisp code
-  ]) ++ [
-    pkgs.notmuch   # From main packages set
-  ])
+    use-package
+  ]))
