9b09ca60ca6ca42c034b7b50c00dd9862fd58262
Jappie Klooster <jappieklooster@hotmail.com>
2018-08-27 13:20:03 +1000

    Pin nixos to a specific channel version.
    Just get random bugs on one machine

--- configuration.nix
+++ configuration.nix
@@ -253,6 +253,9 @@ in {
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "18.03"; # Did you read the comment?
+  system = {
+    nixos.version = "18.03.132507.5f2da7f837e";
+    stateVersion = "18.03"; # Did you read the comment?
+  };
 
 }
