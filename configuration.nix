f5b6db184c953d991ff82f233d12ac8269a6f636
Jappie Klooster <jappieklooster@hotmail.com>
2018-10-07 13:49:30 +0200

    Fix annoying suspend issue

--- configuration.nix
+++ configuration.nix
@@ -26,8 +26,11 @@ in {
     ];
   
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+       loader.systemd-boot.enable = true;
+       loader.efi.canTouchEfiVariables = true;
+       kernelPackages = pkgs.linuxPackages_4_9; # fix supsend maybe?
+  };
 
   networking = {
     hostName = "private-jappie-nixos"; # Define your hostname.
