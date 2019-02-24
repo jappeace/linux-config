42724ee4f97a2169f556e02a40884128ef344e1c
Jappie Klooster <jappieklooster@hotmail.com>
2019-02-24 13:12:09 +0100

    Upgrade maybe solves things

--- configuration.nix
+++ configuration.nix
@@ -364,7 +364,7 @@ in {
     # sudo nix-channel --update
     # sudo nix-channel --list
     # click nixos link, and in title copy over the hash
-    nixos.version = "18.09.1819.76aafbf4bf4";
+    nixos.version = "18.09.2253.753f58d9a42";
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
