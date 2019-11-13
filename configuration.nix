5a6d401e89a6c3c8619cf0f49af3d457278a9ce9
Jappie Klooster <jappieklooster@hotmail.com>
2019-11-13 14:34:42 +0100

    Re-block youtube

--- configuration.nix
+++ configuration.nix
@@ -42,11 +42,14 @@ in
         0.0.0.0 reddit.com
         0.0.0.0 www.reddit.com
         0.0.0.0 trader.degiro.com
-        0.0.0.0 www.linkedin.com
         0.0.0.0 linkedin.com
+        0.0.0.0 www.linkedin.com
         0.0.0.0 twitter.com
         0.0.0.0 www.twitter.com
         0.0.0.0 analytics.google.com
+        0.0.0.0 youtube.com
+        0.0.0.0 www.youtube.com
+        0.0.0.0 youtube.com
         '';
     };
 
