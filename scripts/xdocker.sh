f77efbb01aea3213b08ea06f37df9cffddfd1ab0
Jappie Klooster <jappieklooster@hotmail.com>
2020-05-11 09:25:45 +0200

    Describe how to IB

--- scripts/xdocker.sh
+++ scripts/xdocker.sh
@@ -6,3 +6,9 @@
 # https://github.com/NixOS/nixpkgs/blame/05aa59afa639c188e82e565b63976fa69a7a51a6/nixos/modules/config/pulseaudio.nix#L198
 # http://fabiorehm.com/blog/2014/09/11/running-gui-apps-with-docker/
 docker run -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/jappie:/home/jappie --device /dev/snd -e ALSA_CARD=Generic --env PULSE_SERVER=tcp:172.17.0.1:4713 -it ubuntu-blender
+
+# ib
+#   docker run -d -v /tmp/.X11-unix:/tmp/.X11-unix -it ib bash 
+#    docker exec -it c4609b22e96c   /root/Jts/tws
+# complains about radoen si but doesn't matter, just needs some time to think
+
