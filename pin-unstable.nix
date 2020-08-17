44728f66792ce4aba934e3e1827174d68d76047e
Jappie Klooster <jappieklooster@hotmail.com>
2020-08-17 20:52:29 +0200

    Upgrade pin

--- pin-unstable.nix
+++ pin-unstable.nix
@@ -5,6 +5,6 @@ import (
     {
         url = "https://github.com/NixOS/nixpkgs";
         ref = "master";
-        rev = "b6ee7aa184cc61590915f093b1b6789df7386cbb";
+        rev = "3541e8b4fbf01731580b2f26a3de41200213e6f1";
     }
     ))
