f257e92adaf16369d87b356107462524370c283d
Jappie Klooster <jappieklooster@hotmail.com>
2021-03-19 16:22:40 -0400

    Try fixing wayland

--- overlays/wayland.nix
+++ overlays/wayland.nix
@@ -1,7 +1,9 @@
 # https://github.com/colemickens/nixpkgs-wayland#sway
 { config, lib, pkgs, ... }:
 let
-  rev = "50d9aedc5977e84367f5b14d68bf67ed2c6831df";
+  rev = "37e7b8b772f9c17eb5c593d0f15baf128e330b4e";
+  # rev = "1d3ef245cd7dd11abb16384133a1519c0128d42b";
+  # rev = "50d9aedc5977e84367f5b14d68bf67ed2c6831df";
   # 'rev' could be a git rev, to pin the overla.
   # if you pin, you should use a tool like `niv` maybe, but please consider trying flakes
   url = "https://github.com/colemickens/nixpkgs-wayland/archive/${rev}.tar.gz";
@@ -9,6 +11,12 @@ let
 in
   {
     nixpkgs.overlays = [ waylandOverlay ];
-    environment.systemPackages = [ pkgs.wayvnc ];
+    environment.systemPackages = [
+      pkgs.wldash
+      pkgs.wl-clipboard
+      # pkgs.waybar
+      pkgs.gammastep
+      pkgs.clipman
+    ];
     # ...
   }
