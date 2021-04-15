12cfada83778749faf7b7660aa737fbb1e1b3508
Jappie Klooster <jappieklooster@hotmail.com>
2021-04-15 09:39:40 -0400

    Try fix emacs on i3

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -910,8 +910,10 @@ or GREATER-THAN into an actual Unicode character code. "
     (if (and wl-copy-process (process-live-p wl-copy-process))
         nil ; should return nil if we're the current paste owner
         (shell-command-to-string "wl-paste -n | tr -d \r")))
-  (setq interprogram-cut-function 'wl-copy)
-  (setq interprogram-paste-function 'wl-paste)
+(unless (string= "Failed to connect to a Wayland server" (wl-paste))
+   (setq interprogram-cut-function 'wl-copy)
+   (setq interprogram-paste-function 'wl-paste)
+)
 
 (load "server")
 (unless (server-running-p) (server-start))
