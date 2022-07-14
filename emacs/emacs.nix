eef01227d32623856fda263980498a582662ad5f
Jappie Klooster <jappieklooster@hotmail.com>
2022-07-14 22:39:11 +0200

    progress!! we deleted a bunch of stuff and it still kinda works!!

--- emacs/emacs.nix
+++ emacs/emacs.nix
@@ -6,115 +6,20 @@ let
 
   agsy = (import ./agsy.nix).agda-mode;
 
+        init = pkgs.runCommand "default.el" {} ''
+        mkdir -p $out/share/emacs/site-lisp
+        cp ${pkgs.writeText "default.el" configTxt} $out/share/emacs/site-lisp/default.el
+      '';
+      configTxt = builtins.readFile ./emacs.el;
 
-  # https://sam217pa.github.io/2016/09/02/how-to-build-your-own-spacemacs/
-  myEmacsConfig = pkgs.writeText "default.el" (builtins.readFile ./emacs.el);
-packagedEmacs =
-  emacsWithPackages (epkgs:
-    (let
-      # evilMagit =     epkgs.melpaBuild (
-      # {
-      #   pname = "evil-magit";
-      #   ename = "evil-magit";
-      #   version = "9999";
-      #   recipe = builtins.toFile "recipe" ''
-      #     (evil-magit :fetcher github
-      #     :repo "emacs-evil/evil-magit")
-      #   '';
-
-      #   src = pkgs.fetchFromGitHub {
-      #     owner = "emacs-evil";
-      #     repo = "evil-magit";
-      #     rev = "04a4580c6eadc0e2b821a3525687e74aefc30e84";
-      #     sha256 = "1zckz54pwx63w9j4vlkx0h9mv0p9nbvvynvf9cb6wqm3d0xa4rw2";
-      #   };
-      # });
-      in
-
-  (with epkgs.melpaStablePackages; [
-
-(pkgs.runCommand "default.el" {} ''
-      mkdir -p $out/share/emacs/site-lisp
-      cp ${myEmacsConfig} $out/share/emacs/site-lisp/default.el
-      ln -sf ${./persistent-mode.el} $out/share/emacs/site-lisp/
-      '')
-    avy # jump to word
-    ivy # I think the M-x thing
-    counsel # seach?
-    swiper # other search?
-    which-key # space menu
-    ranger # nav file system
-    company # completion
-    flycheck # squegely lines??
-    powerline # beter status bar (col count, cur line)
-    nix-mode
-    yaml-mode
-    markdown-mode
-    typescript-mode
-    rjsx-mode # better js
-    linum-relative # line numbers are useless, just tell me how much I need to go up
-    rust-mode
-    flx # fuzzy matching
-    counsel-projectile
-    evil-escape
-    elm-mode
-    lua-mode
-    fill-column-indicator # 80 char
-    haskell-mode
-    yasnippet
-    evil
-    ws-butler
-
-
-    # evil-org # broken
-    # dracula-theme
-  ]) ++ (with epkgs.melpaPackages; [
-    wgrep # allow ripgrep find and replace https://github.com/mhayashi1120/Emacs-wgrep
-    # evilMagit
-    magit
-    package-lint
-    dante
-    # ox-reveal # org reveal
-    htmlize
-    general # keybindings
-    monokai-theme
-    use-package # lazy package loading TODO downgrade to stable (custom wan't there)
-    flycheck-rust
-    format-all
-    pretty-symbols
-    flymake-shellcheck
-    direnv
-    # lsp-rust
-    # we bind emacs lsp to whatever lsp's we want
-    # for example haskell: https://github.com/haskell/haskell-ide-engine#using-hie-with-emacs
-    # rust https://github.com/rust-lang-nursery/rls
-    # etc
-    # use hooks to bind haskell to lsp haskell
-    # lspHaskell # https://github.com/emacs-lsp/lsp-haskell
-    # php-mode
-    clojure-mode
-    cider
-    nix-haskell-mode # https://github.com/matthewbauer/nix-haskell-mode
-    nix-sandbox
-    evil-collection
-    smartparens
-    nyan-mode
-    idris-mode
-    shakespeare-mode
-
-    eglot # TODO make works
-    envrc
-    direnv
-
-
-    # lsp-rust https://github.com/emacs-lsp/lsp-rust
-  ]) ++ (with epkgs.elpaPackages; [
-    # ehh
-    cobol-mode
-  ])
-  ++ ([
-    agsy
-  ])
-  ));
 in
-    packagedEmacs
+  # https://sam217pa.github.io/2016/09/02/how-to-build-your-own-spacemacs/
+  pkgs.emacsWithPackagesFromUsePackage {
+        extraEmacsPackages = epkgs: with epkgs; [
+          use-package
+          agsy
+          init
+        ];
+        alwaysEnsure = true;
+        config = configTxt;
+  }
