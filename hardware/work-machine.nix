b246e08caaf9b42da2918f3aeb0a371e0bc62ecc
Jappie Klooster <jappieklooster@hotmail.com>
2023-04-30 11:51:30 +0200

    unmount unused disk

--- hardware/work-machine.nix
+++ hardware/work-machine.nix
@@ -31,17 +31,8 @@
       fsType = "vfat";
     };
 
-  fileSystems."/mnt/nixenc_2021" =
-    { device = "/dev/disk/by-uuid/45c4358e-ae9f-44fb-97b3-87243983cc5b";
-      fsType = "btrfs";
-      options = [ "subvol=nixos" ];
-    };
-
-  boot.initrd.luks.devices."nixenc_2021".device = "/dev/disk/by-uuid/f7a58374-f63e-4bbf-94cb-dbc5f391eced";
-
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/6c5fed09-652b-4a12-8804-2180ab3c0703"; }
-      { device = "/dev/disk/by-uuid/1003f93d-f33a-4906-ab65-f4439d7df731"; }
+    [ { device = "/dev/nvme0n1p2"; }
     ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
