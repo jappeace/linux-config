31c7be128b1d1338000d4cf1c2d91933283ed68e
Jappie Klooster <jappieklooster@hotmail.com>
2024-04-06 19:53:05 +0200

    lol that was my rofi

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,15 @@ let
   );
 
   unstable = (builtins.getFlake "github:nixos/nixpkgs/b263ab4b464169289c25f5ed417aea66ed24189f").legacyPackages.x86_64-linux;
+  rofiWithHoogle = let
+        rofi-hoogle-src = pkgs.fetchFromGitHub {
+          owner = "rebeccaskinner";
+          repo = "rofi-hoogle";
+          rev = "27c273ff67add68578052a13f560a08c12fa5767";
+          sha256 = "09vx9bc8s53c575haalcqkdwy44ys1j8v9k2aaly7lndr19spp8f";
+        };
+        rofi-hoogle = import "${rofi-hoogle-src}/release.nix";
+        in pkgs.rofi.override { plugins = [ rofi-hoogle.rofi-hoogle ]; };
 
   hostdir = pkgs.writeShellScriptBin "hostdir" ''
     ${pkgs.lib.getExe pkgs.python3} -m http.server
@@ -218,6 +227,7 @@ in {
       pkgs.haskellPackages.greenclip
       unstable.nodejs_20 # the one in main is broken, segfautls
       unstable.postgresql
+rofiWithHoogle
       calibre
       audacious
       xclip
