de391552a235e05c9d241d5fa43c4dff6ee5d0cc
Jappie Klooster <jappieklooster@hotmail.com>
2025-07-05 19:15:49 -0400

    fix vbox not booting

--- configuration.nix
+++ configuration.nix
@@ -105,6 +105,18 @@ in
 
   # Use the systemd-boot EFI boot loader.
   boot = {
+    /*
+   solves:
+
+    VirtualBox can't enable the AMD-V extension. Please disable the KVM kernel extension, recompile your kernel and reboot (VERR_SVM_IN_USE).
+    Result Code:
+    NS_ERROR_FAILURE (0x80004005)
+    Component:
+    ConsoleWrap
+    Interface:
+    IConsole {6ac83d89-6ee7-4e33-8ae6-b257b2e81be8}
+    */
+    blacklistedKernelModules = [ "kvm_amd" "kvm" ];
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     plymouth = {
